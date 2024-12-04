d5420cb758e3860150926b66700c5aced020a011
hp <sultonovazizbek73@gmail.com>
2024-12-04 05:38:18 +0500

    get comment

--- lib/ui/pages/home/post_page.dart
+++ lib/ui/pages/home/post_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:twitterclone/models/post.dart';
+import 'package:twitterclone/service/database/database_provider.dart';
 import 'package:twitterclone/ui/widgets/my_post_tile.dart';
 import 'package:twitterclone/utility/navigate_pages.dart';
 
@@ -16,8 +18,14 @@ class PostPage extends StatefulWidget {
 }
 
 class _PostPageState extends State<PostPage> {
+  late final listeningProvider = Provider.of<DatabaseProvider>(context);
+  late final databaseProvider =
+      Provider.of<DatabaseProvider>(context, listen: false);
+
   @override
   Widget build(BuildContext context) {
+    final allComment=listeningProvider.getComments(widget.post.id);
+
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
@@ -33,7 +41,18 @@ class _PostPageState extends State<PostPage> {
             post: widget.post,
             onUserTap: () => goUserPage(context, widget.post.uid),
             onPostTap: () {},
-          )
+          ),
+          allComment.isEmpty?Center(
+            child: Text("No posts yet",),
+          ):ListView.builder(
+              itemCount: allComment.length,
+              physics: const NeverScrollableScrollPhysics(),shrinkWrap: true,
+              itemBuilder: (context,index){
+            final comment=allComment[index];
+            return Container(
+              child: Text(comment.message),
+            );
+          })
         ],
       ),
     );
---
+++ lib/ui/widgets/comment_tile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:twitterclone/models/comment.dart';
+
+class CommentTile extends StatelessWidget {
+  final Comment comment;
+  const CommentTile({super.key,required this.comment});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
